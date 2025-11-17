c7b106f66223d96de5124d7925f4d0312259479f
Varun K1535 <varun.k1535@gmail.com>
2025-11-17 11:40:45 +0530

    added initial catalog-nodejs code

---
+++ data.js
@@ -0,0 +1,8 @@
+let books = [
+      {ID: 1, Name: "Rama II", Author: "Arthur C. Clark", Pages: 281, ImageUrl: "rama.jpg", InStock: 54, Price: 44.23 },
+      {ID: 2, Name: "Exhalation", Author: "Ted Chiang", Pages: 556, ImageUrl: "exhalation.jpg", InStock: 13, Price: 50.99 },
+      {ID: 3, Name: "Traffic Secrets", Author: "Russell Brunson", Pages: 306, ImageUrl: "traffic.jpg", InStock: 65, Price: 18.97 },
+      {ID: 4, Name: "Clean Code", Author: "Robert Martin", Pages: 464, ImageUrl: "clean.jpg", InStock: 15, Price: 87.00 }
+    ];
+
+    module.exports = books;
